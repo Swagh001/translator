5331272013ad6785e9d92bb1b7168f1f194e044c
shailesh wagh <shaileshwagh300@gmail.com>
2024-05-13 17:19:47 +0530

    OG file added

---
+++ src/Component/Home.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+export const Home = () => {
+  let data={
+    name: '',
+    email: '',
+  }
+  const [info, setInfo] = useState({data});
+  const [flag, setFlag] = useState(false);
+
+  const { t, i18n } = useTranslation('global');
+
+  const handleLangChange = (lang) => {
+    i18n.changeLanguage(lang);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFlag(true);
+    setInfo(data);
+  };
+
+  return (
+    <div>
+      <button onClick={() => handleLangChange('en')}>English</button>
+      <br /><br />
+      <button onClick={() => handleLangChange('es')}>Spanish</button>
+      <br /><br />
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          onChange={(e)=>data.name=e.target.value}
+          placeholder={t("formdata.name")}
+        />
+        <br /><br />
+        <input
+          type="text"
+          name="email"
+          placeholder={t("formdata.email")}
+          onChange={(e)=>data.email=e.target.value}
+        />
+        <br /><br />
+        <button type="submit">Submit</button>
+      </form>
+        { flag &&
+          <div>
+            <p>{t('data.name')}: {info.name}</p>
+            <p>{t('data.email')}: {info.email}</p>
+          </div>
+        }
+        
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import i18next from 'i18next';
+import { I18nextProvider } from 'react-i18next';
+import global_en from './Translations/english/en.json';
+import global_es from './Translations/spanish/es.json';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: 'en',
+  resources: {
+    en: {
+      global: global_en,
+    },
+    es: {
+      global: global_es,
+    },
+  },
+});
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
